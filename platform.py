d9d55a609a191cade8a56070de72056f03474800
qitas <qitas@qitas.cn>
2021-03-25 11:47:56 +0800

    update

--- platform.py
+++ platform.py
@@ -1,4 +1,19 @@
+# Copyright 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from platformio.managers.platform import PlatformBase
+
 
 class P11Platform(PlatformBase):
 
@@ -81,7 +96,7 @@ class P11Platform(PlatformBase):
                     "end",
                     "target remote $DEBUG_PORT",
                     "$INIT_BREAK",
-                    "$LOAD_CMD"
+                    "$LOAD_CMDS"
                 ],
                 "port": ":1234",
                 "server": {
