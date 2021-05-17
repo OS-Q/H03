939879a275c284b118ddcd1f058cd04a1284a253
qitas <qitas@qitas.cn>
2021-05-17 14:45:23 +0800

    fix

--- platform.py
+++ platform.py
@@ -1,25 +1,11 @@
-# Copyright 2014-present PlatformIO <contact@platformio.org>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from platformio.managers.platform import PlatformBase
 
 
-class P11Platform(PlatformBase):
+class P111Platform(PlatformBase):
 
     def configure_default_packages(self, variables, targets):
         if not variables.get("board"):
-            return super(P11Platform, self).configure_default_packages(
+            return super(P111Platform, self).configure_default_packages(
                 variables, targets)
 
         build_core = variables.get(
@@ -28,13 +14,6 @@ class P11Platform(PlatformBase):
 
         if "arduino" in variables.get(
                 "pioframework", []) and build_core != "arduino":
-
-            framework_package = "framework-arduino-avr-%s" % build_core.lower()
-            if build_core in ("dtiny", "pro"):
-                framework_package = "framework-arduino-avr-digistump"
-            elif build_core in ("tiny", "tinymodern"):
-                framework_package = "framework-arduino-avr-attiny"
-
             self.frameworks["arduino"]["package"] = framework_package
             self.packages[framework_package]["optional"] = False
             self.packages["framework-arduino-avr"]["optional"] = True
@@ -58,7 +37,7 @@ class P11Platform(PlatformBase):
         if disabled_tool in self.packages and disabled_tool != required_tool:
             del self.packages[disabled_tool]
 
-        return super(P11Platform, self).configure_default_packages(
+        return super(P111Platform, self).configure_default_packages(
             variables, targets)
 
     def on_run_err(self, line):  # pylint: disable=R0201
